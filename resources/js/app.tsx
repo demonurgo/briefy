9e61afc38996283a80cdf75334386f4e268ab3cc
demonurgo <pedroaugusto.cg19@gmail.com>
2026-04-22 12:00:14 -0300

    fix: remove missing bootstrap import from app.tsx

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,6 +1,5 @@
 import './lib/i18n';
 import '../css/app.css';
-import './bootstrap';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
